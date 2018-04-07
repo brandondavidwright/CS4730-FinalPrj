b9de3e8ea73fd549c19edf93ce6ab5702cd7c4ea
David Carlson <37191405+Figureix@users.noreply.github.com>
2018-04-07 09:30:11 -0600

    Create main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+// The following code has been crafted by our own efforts.
+// -some of the code may roll over from previous projects to reach our desired goals for this final project.
+
+// ECE4730 - Final Project - End Goal, LaserTag
+// April 7 2018
+
+
+// Includes for header files and initialization files.
+
+
+
+// main code function
+
+
+
+// Other functions/handlers as needed.
