9f8fd6959827d0524ae0743c614b620d89d6255b
David Carlson <37191405+Figureix@users.noreply.github.com>
2018-04-07 09:57:15 -0600

    Create Init.c

---
+++ Init.c
@@ -0,0 +1,19 @@
+// Initfile file -- to be edit
+
+// Initialize the UART
+void UART_Init(void){
+  SYSCTL_RCGCUART_R |= 0x01;            // activate UART0
+  SYSCTL_RCGCGPIO_R |= 0x01;            // activate port A
+  while((SYSCTL_PRGPIO_R&0x01) == 0){};
+  UART0_CTL_R &= ~UART_CTL_UARTEN;      // disable UART
+  UART0_IBRD_R = 325;                   // IBRD = int(50,000,000 / (16 * 9600)) = int(325.521)
+  UART0_FBRD_R = 33;                    // FBRD = int(0.521) = 33
+                                        // 8 bit word length (no parity bits, one stop bit, FIFOs)
+  UART0_LCRH_R = (UART_LCRH_WLEN_8|UART_LCRH_FEN);
+  UART0_CTL_R |= UART_CTL_UARTEN;       // enable UART
+  GPIO_PORTA_AFSEL_R |= 0x03;           // enable alt funct on PA1-0
+  GPIO_PORTA_DEN_R |= 0x03;             // enable digital I/O on PA1-0
+                                        // configure PA1-0 as UART
+  GPIO_PORTA_PCTL_R = (GPIO_PORTA_PCTL_R&0xFFFFFFF0)+0x00000001;
+  GPIO_PORTA_AMSEL_R &= ~0x03;          // disable analog functionality on PA
+}
