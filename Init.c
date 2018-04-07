f7bce78bcf5be23041f8633d9da107807e5fb41a
David Carlson <37191405+Figureix@users.noreply.github.com>
2018-04-07 11:00:15 -0600

    Update Init.c
    Added extra notes.
    Included PortF init.

--- Init.c
+++ Init.c
@@ -1,10 +1,17 @@
 // Initfile file -- to be edit
 
-// Initialize Everything
-void UART_Init(void){
+// Initialize Everything (PORT A, C, and F)
+// Port A: USB UART usage
+// Port C: Comm between boards
+// Port F: LED lights
+void UARTandGPIO_Init(void){
   SYSCTL_RCGCUART_R |= 0x03;            // activate UART0 and 1
-  SYSCTL_RCGCGPIO_R |= 0x05             // activate port A and C
-  while((SYSCTL_PRGPIO_R&0x05 == 0){};  // Wait for GPIOs be be ready
+  SYSCTL_RCGCGPIO_R |= 0x25;            // activate port A, C, and F
+  while((SYSCTL_PRUART_R&0x03) == 0){}; // Wait for UARTs to be ready
+  while((SYSCTL_PRGPIO_R&0x25) == 0){}; // Wait for GPIOs to be ready
+  
+  
+  // ------------------------------------------------------------------Setup UART0
   UART0_CTL_R &= ~UART_CTL_UARTEN;      // disable UART
   UART0_IBRD_R = 325;                   // IBRD = int(50,000,000 / (16 * 9600)) = int(325.521)
   UART0_FBRD_R = 33;                    // FBRD = int(0.521) = 33
@@ -17,6 +24,8 @@ void UART_Init(void){
   GPIO_PORTA_PCTL_R = (GPIO_PORTA_PCTL_R&0xFFFFFFF0)+0x00000001;
   GPIO_PORTA_AMSEL_R &= ~0x03;          // disable analog functionality on PA
   
+  
+  // ------------------------------------------------------------------Setup UART1
   UART1_CTL_R &= ~UART_CTL_UARTEN;      // disable UART1
   UART1_IBRD_R = 325;                   // IBRD = int(50,000,000 / (16 * 9600)) = int(325.521)
   UART1_FBRD_R = 33;                    // FBRD = int(0.521) = 33
@@ -28,4 +37,16 @@ void UART_Init(void){
                                         // configure PA1-0 as UART
   GPIO_PORTC_PCTL_R = (GPIO_PORTC_PCTL_R&0xFFFFFFF0)+0x00000011;
   GPIO_PORTC_AMSEL_R &= ~0x03;          // disable analog functionality on PA
+  
+  
+  // ------------------------------------------------------------------Setup PortF
+  GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY;    // Allow input
+	GPIO_PORTF_CR_R = 0x1F;               // Commit input
+  GPIO_PORTF_AFSEL_R = 0;
+  GPIO_PORTF_AMSEL_R = 0;
+  GPIO_PORTF_PCTL_R = 0;
+  GPIO_PORTF_DIR_R = 0x0E;              // set bits for LEDs Output
+  GPIO_PORTF_PUR_R = 0x11;              // Pull up bits 0, 4 for input (Switches)
+	GPIO_PORTF_DEN_R = 0x1F;              // Enable all for I/O
+  
 }
